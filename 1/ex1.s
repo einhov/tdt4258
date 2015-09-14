a467a505e0b9e6caedbbfb2b1c30fc114f8e7092
Einar Hov <einhov@gmail.com>
2015-09-14 14:25:31 +0200

    Use semantic register name in ex1.s

--- 1/ex1.s
+++ 1/ex1.s
@@ -9,35 +9,35 @@
 	.thumb_func
 _reset: 
 	// Zero out BSS
-	mov r1, #0
-	ldr r2, =_sbss
-	ldr r3, =_ebss
+	ldr a1, =_sbss
+	ldr a2, =_ebss
+	mov a3, #0
 zero_loop:
-	str r1, [r2], #4
-	cmp r2, r3
+	str a3, [a1], #4
+	cmp a1, a2
 	blo zero_loop
 
 	// Enable GPIO CMU
-	ldr r1, =CMU_BASE
-	mov r2, #0b10000001000000 // GPIO and TIMER1 enabled
-	str r2, [r1, #CMU_HFPERCLKEN0]
+	ldr a1, =CMU_BASE
+	mov a2, #0b10000001000000 // GPIO and TIMER1 enabled
+	str a2, [a1, #CMU_HFPERCLKEN0]
 
 	bl initialise_lfrco
 	bl initialise_controller
 	bl initialise_timer1
 
 	// Enable interrupts
-	ldr r1, =ISER0
-	ldr r2, =0b1100000000010 // (1 << 12) & (1 << 11) & (1 << 1)
-	str r2, [r1]
+	ldr a1, =ISER0
+	ldr a2, =0b1100000000010 // (1 << 12) & (1 << 11) & (1 << 1)
+	str a2, [a1]
 
 .ifdef DOSLEEP
 	// Enable sleep
-	ldr r1, =SCR
-	mov r2, #0x16 // Deep
-	//mov r2, #0x12
-	str r2, [r1]
 sleep_loop:
+	ldr a1, =SCR
+	mov a2, #0x16 // Deep
+	//mov a2, #0x12
+	str a2, [a1]
 	wfi
 	b sleep_loop
 .else
