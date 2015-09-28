a7661b9a736324f9e1d6d9d83bfedc7a3a14a9fe
Einar Hov <einhov@gmail.com>
2015-09-28 13:24:35 +0200

    Add rand function to our CRT

--- 2/src/crt.c
+++ 2/src/crt.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stddef.h>
 
 void *memset(void *s, int c, size_t n) {
@@ -12,4 +13,14 @@ void *memcpy(void *dest, const void *src, size_t n) {
 	for(i = 0; i < n; i++)
 		((char*)dest)[i] = ((char*)src)[i];
 	return dest;
+}
+
+uint32_t rand(void) {
+	const uint32_t a = 22695477;
+	const uint32_t c = 1;
+	const uint32_t m = 2147483648;
+
+	static uint32_t x = 0;
+
+	return x = (a*x+c) % m;
 }
